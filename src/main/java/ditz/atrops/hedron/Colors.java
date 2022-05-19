fe3f186389d873ae9e9a5bdab1516e807f80ecea
d.stueken <d.stueken@conterra.de>
2022-05-19 21:36:43 +0200

    colors

--- src/main/java/ditz/atrops/hedron/Colors.java
+++ src/main/java/ditz/atrops/hedron/Colors.java
@@ -27,78 +27,32 @@ public class Colors {
         return (int) (f-m*Math.floor(f/m));
     }
 
-    static double fmod(double f, int m) {
-        return (f-m*Math.floor(f/m));
-    }
-
     /**
-     * Return a cell index for a 3x3 grid.
-     * Each cell is divided into an upper and a lower cell.
-     * Each cell is then divided into two triangles.
-     *
-     *   +---+---+---+
-     *   |2/3|4/5|6/7|
-     *   |6/7|8/9|0/1|
-     *   |0/1|2/3|4/5|
-     *   +---+---+---+
-     *
+     * Colored basic cell.
+     */
+     
+     static final short[] CELL = {
+             1,1,0,0,0,2,2,2,3,3,3,1,
+             2,2,2,0,0,0,1,1,1,3,3,3,
+             0,2,2,2,3,3,3,1,1,1,0,0,
+             0,0,1,1,1,3,3,3,2,2,2,0,
+             3,3,3,1,1,1,0,0,0,2,2,2,
+             1,3,3,3,2,2,2,0,0,0,1,1
+    };
+     
+     /**
      * @param xi coordinate
      * @param yi coordinate
-     * @return cell index.
+     * @return color index.
      */
 
     static int icell(double xi, double yi) {
-        xi = fmod(xi, 3);
-        yi = fmod(yi, 3);
+        int j = mod(yi, 6);
+        int i = mod(2*xi, 12)/2;
+        int k = mod(yi-xi, 2) + i/3 + j/3;
 
-        int i = (int) xi;
-        int j = (int) yi;
-        int k = mod(yi-xi, 2);
-
-        int ic = 6*j + 2*i + k%2;
-
-        return ic;
-    }
-
-    /**
-     * Colored basic cell.
-     *
-     *  +-----------+-----------+
-     *  |0 1 1 1 2 2 2 3 3 3 0 0|
-     *  |1 1 1 0 0 0 3 3 3 2 2 2|
-     *  |3 3 0 0 0 1 1 1 2 2 2 3|
-     *  |3 2 2 2 1 1 1 0 0 0 3 3|
-     *  |2 2 2 3 3 3 0 0 0 1 1 1|
-     *  |0 0 3 3 3 2 2 2 1 1 1 0|
-     *  +-----------+-----------+
-     */
-
-    static final char[] ICOLOR = {
-            0,0,2,2,2,3,
-            3,3,3,2,2,2,
-            2,3,3,3,1,1,
-    };
-
-    static final List<Color> COLORS = List.of(Color.WHITE, Color.RED, Color.GREEN, Color.BLUE);
-
-    /**
-     * Four basic cell form a 12x12 super cell with permuted colors.
-     *
-     *  |2 |1 |
-     *  |0 |3 |
-     */
-
-    static int icolor(double xi, double yi) {
-        int ic = icell(xi, yi);
-        int color = ICOLOR[ic];
-
-        int i = mod(xi,6)/3;
-        int j = mod(yi,6)/3;
-        int k = 3*(i%2) + 2*(j%2);
-
-        color += k;
-
-        return color & 3;
+        int ic = 12*j + 2*i + k%2;
+        return CELL[ic];
     }
 
     /**
@@ -131,16 +85,23 @@ public class Colors {
         C = 2/(1- T * T);
     }
 
-    Color color(double x, double y) {
+    int icol(double x, double y) {
 
         double xi = C * (x*A + y*B) / size;
         double yi = C * (x*B + y*A) / size;
 
-        int icol = icolor(xi, yi);
-
-        return COLORS.get(icol);
+        return icell(xi, yi);
     }
-    
+
+    int debug(double x, double y) {
+       
+        double g = 6.0/size;
+
+        return icell( g*x, g*y);
+    }
+
+    static final List<Color> COLORS = List.of(Color.YELLOW, Color.RED, Color.GREEN, Color.BLUE);
+
     private final int size;
 
     private final WritableImage image;
@@ -149,16 +110,16 @@ public class Colors {
 
         this.size = size;
 
-        size *= 7;
+        size += 1;
         
         this.image = new WritableImage(size, size);
 
         PixelWriter pw  = image.getPixelWriter();
 
         for(int j=0; j<size; ++j) {
-
             for(int i=0; i<size; ++i) {
-                Color color = color(i,j);
+                int icol = icol(i, j);
+                Color color = COLORS.get(icol);
                 pw.setColor(i, j, color);
             }
         }
@@ -166,7 +127,7 @@ public class Colors {
 
     public static void main(String ... args) throws IOException {
 
-        Colors colors = new Colors(500);
+        Colors colors = new Colors(128);
 
         ImageIO.write(javafx.embed.swing.SwingFXUtils.fromFXImage(colors.image, null), "png", new File("palette.png"));
     }
